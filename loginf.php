e02e908b27b3a02283dac22740ac43f152a39d6a
Alfonso <vargasmesa.alfonso@gmail.com>
2020-02-17 11:12:49 +0100

    Lunes Login

--- loginf.php
+++ loginf.php
@@ -3,21 +3,21 @@
     session_destroy();
 
     
-    $usuario = trim(htmlspecialchars($_REQUEST["usuario"], ENT_QUOTES, "UTF-8"));
-    $contrasena = trim(htmlspecialchars($_REQUEST["contraseña"], ENT_QUOTES, "UTF-8"));
+    $username = trim(htmlspecialchars($_REQUEST["username"], ENT_QUOTES, "UTF-8"));
+    $password = trim(htmlspecialchars($_REQUEST["password"], ENT_QUOTES, "UTF-8"));
 
     $conexion = mysqli_connect("localhost", "root", "1234", "vargasacedo")
     or die("Problemas en la conexion");
     
-    $consulta = "SELECT * FROM administradores WHERE Usuario='$usuario' AND Contrasena='$contrasena'";
+    $consulta = "SELECT * FROM administradores WHERE Usuario='$username' AND Contrasena='$password'";
     
     $registros = mysqli_query($conexion, $consulta) or die(mysqli_error($conexion));
     $count = mysqli_num_rows($registros);
     if ($count != 1) {
-        header('location: inicio.php?error=Usuario o Contraseña Incorrecta');
+        header('location: login.php?error=Usuario o Contraseña Incorrecta');
     } else {
         session_start();
-        $_SESSION['nombreUsuario'] = $usuario; 
+        $_SESSION['nombreUsuario'] = $username; 
         $_SESSION['estado'] = 'Autenticado';
         header('location: administracion.php');
     }
